faffb2b6a12dd16f6d563b805861622ee1f48fc1
Nishat Taaha <73392394+NishatTaaha@users.noreply.github.com>
2021-08-05 15:31:18 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+// initialising
 const nameInput = document.querySelector('.name-input');
 const choiceInput = document.querySelector('.choice-input');
 const authorInput = document.querySelector('.author-input');
 const yearInput = document.querySelector('.year-input')
 const subBtn = document.querySelector('.subBtn');
 
+// submitting form
 subBtn.addEventListener('click', (index) => {
     const nameVal = nameInput.value;
     const choiceVal = choiceInput.value;
     const authorVal = authorInput.value;
     const yearVal = yearInput.value;
     const formAlert= document.querySelector('.formAlert');
+    
+//     for null form
     if (nameVal == '') {
         // alert('Please Enter the your name!!');
         formAlert.style.display = 'block';
@@ -18,6 +22,7 @@ subBtn.addEventListener('click', (index) => {
             formAlert.style.display = 'none';
         }, 1000);
     }
+    
     else if (choiceVal == 'Choose...') {
         // alert('Please Enter the choice value!!');
         formAlert.style.display = 'block';
@@ -26,6 +31,7 @@ subBtn.addEventListener('click', (index) => {
             formAlert.style.display = 'none';
         }, 1000);
     }
+    
     else if (authorVal == '') {
         // alert('Please Enter the author value!!');
         formAlert.style.display = 'block';
@@ -34,6 +40,7 @@ subBtn.addEventListener('click', (index) => {
             formAlert.style.display = 'none';
         }, 1000);
     }
+    
     else if (yearVal == '') {
         // alert('Please Enter the publishing year!!');
         formAlert.style.display = 'block';
@@ -42,13 +49,15 @@ subBtn.addEventListener('click', (index) => {
             formAlert.style.display = 'none';
         }, 1000);
     }
+    
     else {
         let cardContainer = document.querySelector('.cardContainer');
         let cardDiv = document.createElement('div');
         cardDiv.classList.add('card');
         const lastUpdated= document.lastModified;
 
-
+        
+//         adding card divs by submtting form
         cardDiv.innerHTML = `
             <div class="card-body">
                 <p><span class="title font-weight-bold">Your Name: </span>${nameVal}</p>
@@ -60,11 +69,13 @@ subBtn.addEventListener('click', (index) => {
                 <small class="text-muted">${lastUpdated}</small>
             </div>
             <button type="button" class="deleting btn btn-danger" style="background-color: maroon;">Delete</button>`;
+        
         index++;
         cardContainer.appendChild(cardDiv);
 
         deleteFunction();
 
+//         after submitting form nullify form
         nameInput.value = '';
         choiceInput.value = '';
         authorInput.value = '';
@@ -73,12 +84,15 @@ subBtn.addEventListener('click', (index) => {
 });
 
 
+// working of delete button
 function deleteFunction() {
     let deleteBtn = document.querySelectorAll('.deleting');
     const alertCard = document.querySelector('.alertCard ');
+    
     deleteBtn.forEach(function (btn) {
         btn.addEventListener('click', function () {
             btn.parentElement.remove();
+            
             alertCard.style.display = 'block';
             setTimeout(function () {
                 alertCard.style.display = 'none';
@@ -86,6 +100,17 @@ function deleteFunction() {
         });
     });
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
